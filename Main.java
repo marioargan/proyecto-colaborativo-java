4c4d0acb8508e4a3277e3dc20e417e015cee0a84
marioargan <145933384+marioargan@users.noreply.github.com>
2024-12-18 10:07:00 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("¡Hola, mundo desde Alumno A!");
+        System.out.println("¡Hola, mundo desde Alumno B!");
 
     }
 
