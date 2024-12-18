f87df27d06a16fa915bda65edd0edd2ceaf3ce68
Profesor <marioargan@gmail.com>
2024-12-18 11:16:29 +0100

    como no me FUNCIONE MATO A ALGUIEN

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("¡Hola, mundo desde Alumno C!");
+        System.out.println("¡Hola, mundo desde Alumno Juanpa!");
 
     }
 
